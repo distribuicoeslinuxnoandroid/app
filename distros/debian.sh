003a7cbf0591fa6e8acd3130c7e700a0bd22939d
Tiago José <47113005+soutiagojose@users.noreply.github.com>
2024-02-14 02:56:02 -0300

    v0.0.2

---
+++ distros/debian.sh
@@ -0,0 +1,31 @@
+#!/data/data/com.termux/files/usr/bin/bash
+#📥
+echo -e "\e[44m
+
+
+
+
+                                        bbbbbbbb                                                       
+DDDDDDDDDDDDD                           b::::::b              iiii                                     
+D::::::::::::DDD                        b::::::b             i::::i                                    
+D:::::::::::::::DD                      b::::::b              iiii                                     
+DDD:::::DDDDD:::::D                      b:::::b                                                       
+  D:::::D    D:::::D     eeeeeeeeeeee    b:::::bbbbbbbbb    iiiiiii   aaaaaaaaaaaaa  nnnn  nnnnnnnn    
+  D:::::D     D:::::D  ee::::::::::::ee  b::::::::::::::bb  i:::::i   a::::::::::::a n:::nn::::::::nn  
+  D:::::D     D:::::D e::::::eeeee:::::eeb::::::::::::::::b  i::::i   aaaaaaaaa:::::an::::::::::::::nn 
+  D:::::D     D:::::De::::::e     e:::::eb:::::bbbbb:::::::b i::::i            a::::ann:::::::::::::::n
+  D:::::D     D:::::De:::::::eeeee::::::eb:::::b    b::::::b i::::i     aaaaaaa:::::a  n:::::nnnn:::::n
+  D:::::D     D:::::De:::::::::::::::::e b:::::b     b:::::b i::::i   aa::::::::::::a  n::::n    n::::n
+  D:::::D     D:::::De::::::eeeeeeeeeee  b:::::b     b:::::b i::::i  a::::aaaa::::::a  n::::n    n::::n
+  D:::::D    D:::::D e:::::::e           b:::::b     b:::::b i::::i a::::a    a:::::a  n::::n    n::::n
+DDD:::::DDDDD:::::D  e::::::::e          b:::::bbbbbb::::::bi::::::ia::::a    a:::::a  n::::n    n::::n
+D:::::::::::::::DD    e::::::::eeeeeeee  b::::::::::::::::b i::::::ia:::::aaaa::::::a  n::::n    n::::n
+D::::::::::::DDD       ee:::::::::::::e  b:::::::::::::::b  i::::::i a::::::::::aa:::a n::::n    n::::n
+DDDDDDDDDDDDD            eeeeeeeeeeeeee  bbbbbbbbbbbbbbbb   iiiiiiii  aaaaaaaaaa  aaaa nnnnnn    nnnnnn 
+                                   
+
+
+
+
+\e[0m"
+clear

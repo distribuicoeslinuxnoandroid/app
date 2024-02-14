192d59b5818adf23dbe684150708b8f8934d098e
AllyTiago <47113005+allytiago@users.noreply.github.com>
2024-02-14 02:29:48 -0300

    v0.0.1

---
+++ start.sh
@@ -0,0 +1,33 @@
+#!/data/data/com.termux/files/usr/bin/bash
+#🚀
+pkg install wget dialog -y
+
+extralink="https://raw.githubusercontent.com/distribuicoeslinuxnoandroid/app"
+
+export USER=$(whoami)
+HEIGHT=0
+WIDTH=0
+CHOICE_HEIGHT=5
+MENU="Escolha algumas das seguintes opções: \n \nChoose any of the following options: "
+export PORT=1
+
+OPTIONS=(1 "Ubuntu")
+
+CHOICE=$(dialog --clear \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+1)
+echo "Ubuntu"
+wget --tries=20 $extralink/distros/ubuntu.sh -O start-distro.sh
+;;
+
+esac
+
+chmod +x start-distro.sh
+bash start-distro.sh

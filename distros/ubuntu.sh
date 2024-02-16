76f486b0e1bc8970d5ec858333f37e05b0755692
Tiago José <47113005+soutiagojose@users.noreply.github.com>
2024-02-16 00:27:38 -0300

    v0.0.14 * Tentando resolver o problema da escolha de versão * Retirado o echo com o nome ubuntu

--- distros/ubuntu.sh
+++ distros/ubuntu.sh
@@ -1,35 +1,24 @@
 #!/data/data/com.termux/files/usr/bin/bash
 #📥
-echo -e "\e[44m
-
-
-
-
-                     bbbbbbbb                                                                                       
-UUUUUUUU     UUUUUUUUb::::::b                                                       tttt                            
-U::::::U     U::::::Ub::::::b                                                    ttt:::t                            
-U::::::U     U::::::Ub::::::b                                                    t:::::t                            
-UU:::::U     U:::::UU b:::::b                                                    t:::::t                            
- U:::::U     U:::::U  b:::::bbbbbbbbb    uuuuuu    uuuuuunnnn  nnnnnnnn    ttttttt:::::ttttttt    uuuuuu    uuuuuu  
- U:::::D     D:::::U  b::::::::::::::bb  u::::u    u::::un:::nn::::::::nn  t:::::::::::::::::t    u::::u    u::::u  
- U:::::D     D:::::U  b::::::::::::::::b u::::u    u::::un::::::::::::::nn t:::::::::::::::::t    u::::u    u::::u  
- U:::::D     D:::::U  b:::::bbbbb:::::::bu::::u    u::::unn:::::::::::::::ntttttt:::::::tttttt    u::::u    u::::u  
- U:::::D     D:::::U  b:::::b    b::::::bu::::u    u::::u  n:::::nnnn:::::n      t:::::t          u::::u    u::::u  
- U:::::D     D:::::U  b:::::b     b:::::bu::::u    u::::u  n::::n    n::::n      t:::::t          u::::u    u::::u  
- U:::::D     D:::::U  b:::::b     b:::::bu::::u    u::::u  n::::n    n::::n      t:::::t          u::::u    u::::u  
- U::::::U   U::::::U  b:::::b     b:::::bu:::::uuuu:::::u  n::::n    n::::n      t:::::t    ttttttu:::::uuuu:::::u  
- U:::::::UUU:::::::U  b:::::bbbbbb::::::bu:::::::::::::::uun::::n    n::::n      t::::::tttt:::::tu:::::::::::::::uu
-  UU:::::::::::::UU   b::::::::::::::::b  u:::::::::::::::un::::n    n::::n      tt::::::::::::::t u:::::::::::::::u
-    UU:::::::::UU     b:::::::::::::::b    uu::::::::uu:::un::::n    n::::n        tt:::::::::::tt  uu::::::::uu:::u
-      UUUUUUUUU       bbbbbbbbbbbbbbbb       uuuuuuuu  uuuunnnnnn    nnnnnn          ttttttttttt      uuuuuuuu  uuuu
+#                     bbbbbbbb                                                                                       
+# UUUUUUUU     UUUUUUUUb::::::b                                                       tttt                            
+# U::::::U     U::::::Ub::::::b                                                    ttt:::t                            
+# U::::::U     U::::::Ub::::::b                                                    t:::::t                            
+# UU:::::U     U:::::UU b:::::b                                                    t:::::t                            
+#  U:::::U     U:::::U  b:::::bbbbbbbbb    uuuuuu    uuuuuunnnn  nnnnnnnn    ttttttt:::::ttttttt    uuuuuu    uuuuuu  
+#  U:::::D     D:::::U  b::::::::::::::bb  u::::u    u::::un:::nn::::::::nn  t:::::::::::::::::t    u::::u    u::::u  
+#  U:::::D     D:::::U  b::::::::::::::::b u::::u    u::::un::::::::::::::nn t:::::::::::::::::t    u::::u    u::::u  
+#  U:::::D     D:::::U  b:::::bbbbb:::::::bu::::u    u::::unn:::::::::::::::ntttttt:::::::tttttt    u::::u    u::::u  
+#  U:::::D     D:::::U  b:::::b    b::::::bu::::u    u::::u  n:::::nnnn:::::n      t:::::t          u::::u    u::::u  
+#  U:::::D     D:::::U  b:::::b     b:::::bu::::u    u::::u  n::::n    n::::n      t:::::t          u::::u    u::::u  
+#  U:::::D     D:::::U  b:::::b     b:::::bu::::u    u::::u  n::::n    n::::n      t:::::t          u::::u    u::::u  
+#  U::::::U   U::::::U  b:::::b     b:::::bu:::::uuuu:::::u  n::::n    n::::n      t:::::t    ttttttu:::::uuuu:::::u  
+#  U:::::::UUU:::::::U  b:::::bbbbbb::::::bu:::::::::::::::uun::::n    n::::n      t::::::tttt:::::tu:::::::::::::::uu
+#   UU:::::::::::::UU   b::::::::::::::::b  u:::::::::::::::un::::n    n::::n      tt::::::::::::::t u:::::::::::::::u
+#     UU:::::::::UU     b:::::::::::::::b    uu::::::::uu:::un::::n    n::::n        tt:::::::::::tt  uu::::::::uu:::u
+#       UUUUUUUUU       bbbbbbbbbbbbbbbb       uuuuuuuu  uuuunnnnnn    nnnnnn          ttttttttttt      uuuuuuuu  uuuu
                                    
 
-
-
-
-\e[0m"
-sleep 2
-clear
 
 pkg install wget curl proot tar dialog -y
 folder="ubuntu22-fs"

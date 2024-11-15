0182f35f50c6eb7795cba33755bc2d580cab2450
Tiago José <47113005+soutiagojose@users.noreply.github.com>
2024-11-15 00:49:00 +0000

    v0.0.56
    * Bugs resolvidos

--- distros/ubuntu.sh
+++ distros/ubuntu.sh
@@ -600,6 +600,8 @@ apt install sudo wget -y > /dev/null 2>&1
 
 bash ~/locale*.sh
 
+apt update
+
 rm -rf ~/locale*.sh
 rm -rf ~/.bash_profile
 exit" > $folder/root/.bash_profile 
@@ -718,43 +720,45 @@ if [ -f "fixed_variables.sh" ]; then
 	chmod +x fixed_variables.sh
 	source fixed_variables.sh
 	else
-		(
-				echo 0  # Inicia em 61%
-				wget --tries=20 "${extralink}/config/fixed_variables.sh" --progress=dot:giga 2>&1 | while read -r line; do
-					# Extraindo a porcentagem do progresso do wget
-					if [[ $line =~ ([0-9]+)% ]]; then
-						percent=${BASH_REMATCH[1]}
-						echo $percent  # Atualiza a barra de progresso
-					fi
-				done
 
-				echo 50  # Finaliza em 80%
-			) | whiptail --gauge "${label_progress}" 0 0 0
+		(
+			echo 0  # Inicia em 0%
+			wget --tries=20  "${extralink}/config/fixed_variables.sh" -O > /dev/null --progress=dot:giga 2>&1 | while read -r line; do
+				# Extraindo a porcentagem do progresso do wget
+				if [[ $line =~ ([0-9]+)% ]]; then
+					percent=${BASH_REMATCH[1]}
+					echo $percent  # Atualiza a barra de progresso
+				fi
+			done
+			sleep 1
+			echo 50  # Finaliza em 100%
+		) | whiptail --gauge "${label_progress}" 0 0 0
 
 		chmod +x fixed_variables.sh
 		source fixed_variables.sh
 fi
 
 if grep -q "LANG=pt_BR.UTF-8" ~/.bashrc; then # Se houver o LANG de idioma dentro do bashrc
-	export LANGUAGE=pt_BR.UTF-8
-	export LANG=pt_BR.UTF-8
-	export LC_ALL=pt_BR.UTF-8
 	if [ -f "l10n_pt-BR.sh" ]; then # verifica se existe o arquivo
 		chmod +x l10n_pt-BR.sh
 		source l10n_pt-BR.sh
 		else
-			(
-				echo 51  # Inicia em 61%
-				wget --tries=20 "${extralink}/config/locale/l10n_pt_BR.sh" --progress=dot:giga 2>&1 | while read -r line; do
-					# Extraindo a porcentagem do progresso do wget
-					if [[ $line =~ ([0-9]+)% ]]; then
-						percent=${BASH_REMATCH[1]}
-						echo $percent  # Atualiza a barra de progresso
-					fi
-				done
 
-				echo 100  # Finaliza em 80%
-			) | whiptail --gauge "${label_progress}" 0 0 0
+
+			(
+			echo 51  # Inicia em 0%
+			wget --tries=20  "${extralink}/config/locale/l10n_pt_BR.sh" -O > /dev/null --progress=dot:giga 2>&1 | while read -r line; do
+				# Extraindo a porcentagem do progresso do wget
+				if [[ $line =~ ([0-9]+)% ]]; then
+					percent=${BASH_REMATCH[1]}
+					echo $percent  # Atualiza a barra de progresso
+				fi
+			done
+			sleep 1
+			echo 100  # Finaliza em 100%
+		) | whiptail --gauge "${label_progress}" 0 0 0
+
+
 			chmod +x l10n_pt-BR.sh
 			source l10n_pt-BR.sh
 	fi

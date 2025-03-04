37a96720a9fea6211def7e018c1c4f09bef2416c
Tiago José <47113005+soutiagojose@users.noreply.github.com>
2025-03-04 23:01:48 +0000

    v0.0.64.8.6
    * Correções de possiveis falhas

--- distros/debian.sh
+++ distros/debian.sh
@@ -582,6 +582,6 @@ rm -rf ~/.bash_profile' > $folder/root/.bash_profile
 #rm -rf ~/fixed_variables.sh
 
 # Cria uma gui de inicialização
-sed -i '\|command+=" /bin/bash --login"|a command+=" -b /data/data/com.termux/files/home/debian-stable/usr/local/bin/startvncserver"' $bin
+sed -i '\|command+=" /bin/bash --login"|a command+=" -b /usr/local/bin/startvncserver"' $bin
 
 bash $bin

a5b45916c1ae101d820761ec7f4dd457db3b06b7
Tiago José <47113005+soutiagojose@users.noreply.github.com>
2025-04-14 00:15:35 -0300

    🚀 Central de controle AnDistro
    * Correção da barra de progresso
    * Um único arquivo para fazer a instalação, desinstalação das distribuições linux.
    
    * Atualiza automáticamente;
    *Baixa os pacotes necessários para iniciar os trabalhos;
    
    * Instalar
    - Debian;
    - Ubuntu;
    
    *Desinstalar
    - Debian;
    - Ubuntu;

--- config/fixed_variables.sh
+++ config/fixed_variables.sh
@@ -43,16 +43,16 @@ export NEWT_COLORS="window=,white border=black,white title=black,white textbox=b
 # update_progress() precisa ser definido antes de ser usado
 
 
-#estagio=33 #33% completos
+estagio=10
 update_progress() {
     percent=$1
-    stage=$((percent / $estagio + 1)) #precisa criar o "estagio=<número>" no código onde será executado"
+    stage=$((percent / $estagio + 1))
     bar_length=30
     filled_length=$((percent * bar_length / 100))
     empty_length=$((bar_length - filled_length))
 
     filled_bar=$(printf "%${filled_length}s" | tr " " "=")
     empty_bar=$(printf "%${empty_length}s" | tr " " " ")
-
-    printf "\r%d/3 [%s%s] %3d%%" "$stage" "$filled_bar" "$empty_bar" "$percent"
+    
+    printf "\r%d/%d [%s%s] %3d%%" "$stage" "$total_stages" "$filled_bar" "$empty_bar" "$percent"
 }
